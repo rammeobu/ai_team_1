c6ffeab1d1ce5d212d7d4f54558c0c5713cb948e
_.Cloudhigh <wnsldjtksid@naver.com>
2026-07-08 10:09:14 +0900

    add new files

---
+++ src/main/java/com/bootcamp/bootcamp/entity/userentity.java
@@ -0,0 +1,12 @@
+package com.bootcamp.bootcamp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.websocket.server.ServerEndpoint;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class userentity {
+}
