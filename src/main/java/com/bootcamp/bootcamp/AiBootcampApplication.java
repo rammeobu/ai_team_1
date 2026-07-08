56bb9aa5ea9b726bd144b9627727d589b92d9048
_.Cloudhigh <wnsldjtksid@naver.com>
2026-07-08 09:38:49 +0900

    백엔드 기본 틀

---
+++ src/main/java/com/bootcamp/bootcamp/AiBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBootcampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiBootcampApplication.class, args);
+    }
+
+}
